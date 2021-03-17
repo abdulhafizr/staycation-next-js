5c66fb7d7194f2ee292e47283a97e82bf9296c82
ahr <abdulhafizr>
2021-03-17 20:24:59 +0700

    | Download all assets project

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,10 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Staycation | Forget Busy Work, Start Next Vacation</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
 
     </div>
   )
