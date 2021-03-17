f5f43a5d22f0c869a46fcfdce05cb771982985b8
ahr <abdulhafizr>
2021-03-17 21:30:21 +0700

    | Make Header Component

---
+++ components/Header.js
@@ -0,0 +1,30 @@
+import {useRouter} from 'next/router'
+import styles from '../styles/Home.module.scss'
+
+export default function Header() {
+    const router = useRouter()
+    return (
+        <header className={styles['l-header']}>
+            <nav className={styles['nav']}>
+                <div>
+                    <h1 className={styles['nav__brand']}>Stay<span>cation.</span></h1>
+                </div>
+
+                <ul className={styles['nav__menu']}>
+                    <li className={styles['nav__item']}>
+                        <a href="#" className={`${styles['nav__link']} active`}>Home</a>
+                    </li>
+                    <li className={styles['nav__item']}>
+                        <a href="#" className={styles['nav__link']}>Browse by</a>
+                    </li>
+                    <li className={styles['nav__item']}>
+                        <a href="#" className={styles['nav__link']}>Stories</a>
+                    </li>
+                    <li className={styles['nav__item']}>
+                        <a href="#" className={styles['nav__link']}>Agents</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import {Header} from '../components'
 
 export default function Home() {
   return (
@@ -8,7 +9,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-
+      <Header />
     </div>
   )
 }
