15c92a36aae79ab10815c8108c8d3634619927a7
ahr <abdulhafizr>
2021-03-18 07:37:17 +0700

    | Refactor style from CSS Module to Global CSS

--- components/Header.js
+++ components/Header.js
@@ -1,27 +1,26 @@
 import {useRouter} from 'next/router'
-import styles from '../styles/Home.module.scss'
 
 export default function Header() {
     const router = useRouter()
     return (
-        <header className={styles['l-header']}>
-            <nav className={`${styles['nav']} container`}>
+        <header className="l-header">
+            <nav className="container nav">
                 <div>
-                    <h1 className={styles['nav__brand']}>Stay<span>cation.</span></h1>
+                    <h1 className="nav__brand">Stay<span>cation.</span></h1>
                 </div>
 
-                <ul className={styles['nav__menu']}>
-                    <li className={styles['nav__item']}>
-                        <a href="#" className={`${styles['nav__link']} active`}>Home</a>
+                <ul className="nav__menu">
+                    <li className="nav__item">
+                        <a href="#" className="nav__link active">Home</a>
                     </li>
-                    <li className={styles['nav__item']}>
-                        <a href="#" className={styles['nav__link']}>Browse by</a>
+                    <li className="nav__item">
+                        <a href="#" className="nav__link">Browse by</a>
                     </li>
-                    <li className={styles['nav__item']}>
-                        <a href="#" className={styles['nav__link']}>Stories</a>
+                    <li className="nav__item">
+                        <a href="#" className="nav__link">Stories</a>
                     </li>
-                    <li className={styles['nav__item']}>
-                        <a href="#" className={styles['nav__link']}>Agents</a>
+                    <li className="nav__item">
+                        <a href="#" className="nav__link">Agents</a>
                     </li>
                 </ul>
             </nav>
--- components/MostPick.js
+++ components/MostPick.js
@@ -1,75 +1,74 @@
 import Image from 'next/image'
-import styles from '../styles/Home.module.scss';
 
 export default function MostPick() {
     return (
-        <div className={styles['mostpick']}>
-            <h1 className={styles['mostpick__title']}>Most Picked</h1>
+        <div className="mostpick">
+            <h1 className="mostpick__title">Most Picked</h1>
             
-            <div className={styles['mostpick__gallery']}>
-                <div className={styles['mostpick__gallery-item']}>
-                    <span className={styles['mostpick__gallery-item-badge']}>$50 per night</span>
+            <div className="mostpick__gallery">
+                <div className="mostpick__gallery-item">
+                    <span className="mostpick__gallery-item-badge">$50 per night</span>
                     <Image 
                         src="/dummys/mp_1.png"
                         layout="fill"
                         objectFit="cover"
-                        className={styles['mostpick__gallery-item-image']}
+                        className="mostpick__gallery-item-image"
                     />
-                    <div className={styles['mostpick__gallery-item-caption']}>
-                        <p className={styles['mostpick__gallery-item-caption-label']}>Blue Origin Fams</p>
-                        <p className={styles['mostpick__gallery-item-caption-location']}>Jakarta, Indonesia</p>
+                    <div className="mostpick__gallery-item-caption">
+                        <p className="mostpick__gallery-item-caption-label">Blue Origin Fams</p>
+                        <p className="mostpick__gallery-item-caption-location">Jakarta, Indonesia</p>
                     </div>
                 </div>
-                <div className={styles['mostpick__gallery-item']}>
-                    <span className={styles['mostpick__gallery-item-badge']}>$50 per night</span>
+                <div className="mostpick__gallery-item">
+                    <span className="mostpick__gallery-item-badge">$50 per night</span>
                     <Image 
                         src="/dummys/mp_2.png"
                         layout="fill"
                         objectFit="cover"
-                        className={styles['mostpick__gallery-item-image']}
+                        className="mostpick__gallery-item-image"
                     />
-                    <div className={styles['mostpick__gallery-item-caption']}>
-                        <p className={styles['mostpick__gallery-item-caption-label']}>Blue Origin Fams</p>
-                        <p className={styles['mostpick__gallery-item-caption-location']}>Jakarta, Indonesia</p>
+                    <div className="mostpick__gallery-item-caption">
+                        <p className="mostpick__gallery-item-caption-label">Blue Origin Fams</p>
+                        <p className="mostpick__gallery-item-caption-location">Jakarta, Indonesia</p>
                     </div>
                 </div>
-                <div className={styles['mostpick__gallery-item']}>
-                    <span className={styles['mostpick__gallery-item-badge']}>$50 per night</span>
+                <div className="mostpick__gallery-item">
+                    <span className="mostpick__gallery-item-badge">$50 per night</span>
                     <Image 
                         src="/dummys/mp_3.png"
                         layout="fill"
                         objectFit="cover"
-                        className={styles['mostpick__gallery-item-image']}
+                        className="mostpick__gallery-item-image"
                     />
-                    <div className={styles['mostpick__gallery-item-caption']}>
-                        <p className={styles['mostpick__gallery-item-caption-label']}>Blue Origin Fams</p>
-                        <p className={styles['mostpick__gallery-item-caption-location']}>Jakarta, Indonesia</p>
+                    <div className="mostpick__gallery-item-caption">
+                        <p className="mostpick__gallery-item-caption-label">Blue Origin Fams</p>
+                        <p className="mostpick__gallery-item-caption-location">Jakarta, Indonesia</p>
                     </div>
                 </div>
-                <div className={styles['mostpick__gallery-item']}>
-                    <span className={styles['mostpick__gallery-item-badge']}>$50 per night</span>
+                <div className="mostpick__gallery-item">
+                    <span className="mostpick__gallery-item-badge">$50 per night</span>
                     <Image 
                         src="/dummys/mp_4.png"
                         layout="fill"
                         objectFit="cover"
-                        className={styles['mostpick__gallery-item-image']}
+                        className="mostpick__gallery-item-image"
                     />
-                    <div className={styles['mostpick__gallery-item-caption']}>
-                        <p className={styles['mostpick__gallery-item-caption-label']}>Blue Origin Fams</p>
-                        <p className={styles['mostpick__gallery-item-caption-location']}>Jakarta, Indonesia</p>
+                    <div className="mostpick__gallery-item-caption">
+                        <p className="mostpick__gallery-item-caption-label">Blue Origin Fams</p>
+                        <p className="mostpick__gallery-item-caption-location">Jakarta, Indonesia</p>
                     </div>
                 </div>
-                <div className={styles['mostpick__gallery-item']}>
-                    <span className={styles['mostpick__gallery-item-badge']}>$50 per night</span>
+                <div className="mostpick__gallery-item">
+                    <span className="mostpick__gallery-item-badge">$50 per night</span>
                     <Image 
                         src="/dummys/mp_5.png"
                         layout="fill"
                         objectFit="cover"
-                        className={styles['mostpick__gallery-item-image']}
+                        className="mostpick__gallery-item-image"
                     />
-                    <div className={styles['mostpick__gallery-item-caption']}>
-                        <p className={styles['mostpick__gallery-item-caption-label']}>Blue Origin Fams</p>
-                        <p className={styles['mostpick__gallery-item-caption-location']}>Jakarta, Indonesia</p>
+                    <div className="mostpick__gallery-item-caption">
+                        <p className="mostpick__gallery-item-caption-label">Blue Origin Fams</p>
+                        <p className="mostpick__gallery-item-caption-location">Jakarta, Indonesia</p>
                     </div>
                 </div>
             </div>
