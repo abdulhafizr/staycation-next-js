5fec1daefa12be4299103b453e4b0544c037a8ba
ahr <abdulhafizr>
2021-03-18 15:36:35 +0700

    | Make Footer Section

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,37 @@
 export default function Footer() {
     return (
-        <footer>
-            Footer
+        <footer className="footer">
+            <div className="footer__top">
+                <div className="footer__brand">
+                    <h1 className="footer__brand-title">Stay<span>cation.</span></h1>
+                    <p className="footer__item">We kaboom your beauty holiday instantly and memorable.</p>
+                </div>
+                <div className="footer__a">
+                    <h1 className="footer__title">For Beginners</h1>
+                    <div className="footer__item-wrapper">
+                        <p className="footer__item">New Account</p>
+                        <p className="footer__item">Start Booking a Room</p>
+                        <p className="footer__item">Use Payments</p>
+                    </div>
+                </div>
+                <div className="footer__b">
+                    <h1 className="footer__title">Explore Us</h1>
+                    <div className="footer__item-wrapper">
+                        <p className="footer__item">Our Careers</p>
+                        <p className="footer__item">Privacy</p>
+                        <p className="footer__item">Terms & Conditions</p>
+                    </div>
+                </div>
+                <div className="footer__c">
+                    <h1 className="footer__title">Connect Us</h1>
+                    <div className="footer__item-wrapper">
+                        <p className="footer__item">support@staycation.id</p>
+                        <p className="footer__item">021 - 2208 - 1996</p>
+                        <p className="footer__item">Staycation, Kemang, Jakarta</p>
+                    </div>
+                </div>
+            </div>
+            <p className="footer__bottom">Copyright 2019 • All rights reserved • Staycation</p>
         </footer>
     )
 }
